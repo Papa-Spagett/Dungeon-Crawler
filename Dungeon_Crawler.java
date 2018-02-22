6289395d074d3475677c38d3688889578029cb07
Thomas Faucette-Boyd <36012017+Papa-Spagett@users.noreply.github.com>
2018-02-22 17:30:59 -0500

    Add files via upload

---
+++ Dungeon_Crawler.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dungeon_crawler;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+/**
+ *
+ * @author Big Thomas
+ */
+public class Dungeon_Crawler {
+
+    /**
+     * @param args the command line arguments
+     */
+   public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Move by typing left or right");
+        /*starting instruction
+        
+        */
+       System.out.println("Get to the end of the Dungeon. Which way do you want to move? ");
+        /*the first movement 
+       
+       */
+       
+       
+   }
+    private class Dungeons {
+
+        String[] Dugeon = new String[3];
+    /** Creates a new instance of Array */
+    public Dungeons() {
+        Dugeon[1]= "hero";
+    }
+}
+    public static void Hero(String[] args){
+        
+    }
+}
