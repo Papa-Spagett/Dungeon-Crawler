53214e7d3f509a0789a669846fb68397a7358c21
Thomas Faucette-Boyd <36012017+Papa-Spagett@users.noreply.github.com>
2018-02-22 18:01:43 -0500

    Add files via upload
    Enemies and Hero are in Place

--- Dungeon_Crawler.java
+++ Dungeon_Crawler.java
@@ -6,6 +6,7 @@
 package dungeon_crawler;
 
 import java.util.ArrayList;
+import java.util.Random;
 import java.util.Scanner;
 /**
  *
@@ -17,7 +18,55 @@ public class Dungeon_Crawler {
      * @param args the command line arguments
      */
    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+       //For inputs
+        Scanner input = new Scanner(System.in);
+        //For Random Number Generation
+        Random rand = new Random();
+        
+        //Variables for Monsters
+        String[] Mobs = {"Wild Meatball Goblin", "Linguinie Troll", " Ravioli Rouge", "Wet Noodle Wizard", "Grand Overlord Master King Spaghett"}; 
+        int maxMobHP = 100;
+        int maxMobDmg = 20;
+        
+        //Variables for the Player
+        int Hp = 250;
+        int PlayerDmg = 40;
+        int NumHpPotions = 3;
+        int HPPotionHeal = 50;
+        
+        //So the game continues to run
+        boolean playing = true; 
+        
+        
+        System.out.println( "Enter Spaghett's Lair! " );
+        
+        
+        //label to tell game to start from here
+        GAME:
+        while (playing) {
+            System.out.println( "++++++++++++++++++++++++++++++++++++++");
+            
+            int MobHp = rand.nextInt(maxMobHP);
+            String Mob = Mobs[rand.nextInt(Mobs.length)];
+            System.out.println("\t# " + Mob + "has shown up! #\n");
+            
+            
+            while(MobHp> 0){
+            System.out.println("\t PlayerHP: ");
+            System.out.println("\t" + Mob+ "'s HP: ");
+            System.out.println("");
+            System.out.println("");
+            System.out.println("");
+        }
+                    }
+               
+        
+        
+        
+        
+       
+        
+        
         System.out.println("Move by typing left or right");
         /*starting instruction
         
@@ -26,18 +75,20 @@ public class Dungeon_Crawler {
         /*the first movement 
        
        */
-       
-       
    }
-    private class Dungeons {
-
-        String[] Dugeon = new String[3];
-    /** Creates a new instance of Array */
-    public Dungeons() {
-        Dugeon[1]= "hero";
-    }
 }
-    public static void Hero(String[] args){
-        
-    }
-}
+       
+       
+   
+//    private class Dungeons {
+//
+//        String[] Dugeon = new String[3];
+//    /** Creates a new instance of Array */
+//    public Dungeons() {
+//        Dugeon[1]= "hero";
+//    }
+//}
+//    public static void Hero(String[] args){
+//        
+//    }
+//}
