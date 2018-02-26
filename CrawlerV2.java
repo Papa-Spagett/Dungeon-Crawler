e442509c7b105548adc0135db4aad5be9a7e3b76
Thomas Faucette-Boyd <36012017+Papa-Spagett@users.noreply.github.com>
2018-02-26 18:40:44 -0500

    Add files via upload

---
+++ CrawlerV2.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package crawlerV2;
+
+/**
+ *
+ * @author Big Thomas
+ */
+public class CrawlerV2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+      
+        
+            class Dungeon{     
+            }
+            class Enemy{
+            }
+            class Boss{
+            
+            }
+            class Hero{
+            
+            } 
+        }
+                
+    }
+    
+
