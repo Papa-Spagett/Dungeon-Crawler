7c40bc9aa42447423a3305c6571fadd765122007
Thomas Faucette-Boyd <36012017+Papa-Spagett@users.noreply.github.com>
2018-05-20 23:51:20 -0400

    Add files via upload
    Finished Version of Project one Server-Client communication

--- Client.java
+++ Client.java
@@ -1,30 +1,89 @@
-
-import java.io.IOException;
-
-import java.net.*;
-import java.util.Scanner;
-
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+package Classes;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.util.Scanner;
 
 /**
  *
  * @author Big Thomas
  */
 public class Client {
-     public static void main(String args[]) throws IOException{
-      DatagramSocket sc = new DatagramSocket();
-      Scanner scan = new Scanner(System.in);
-  
-      System.out.println("Please Type your greeting:  ");
-      String greeting = scan.nextLine();
-      InetAddress ip = InetAddress.getByName("131.118.66.187");
+   
       
-      DatagramPacket pkt = new DatagramPacket(greeting.getBytes(),greeting.length(),ip,58778);
+
+ public static void main(String[] args)throws IOException {
+        Dungeon d = new Dungeon();
+        Scanner in = new Scanner(System.in);
+        DatagramSocket sc = new DatagramSocket();
+      Scanner scan = new Scanner(System.in);
+   InetAddress ip = InetAddress.getByName("131.118.66.187");
+      
+   
+
+//So the game continues to run
+        boolean playing = true; 
+        
+        
+     System.out.println( "Enter Spaghett's Lair! " );
+        d.populate();
+        
+        //label to tell game to start from here
+        GAME:
+        while (playing) {
+            System.out.println( "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+            
+    
+     System.out.println( "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+        System.out.println(" Forward: Keep battling the forces of evil.");
+        System.out.println(" Leave: Be a wuss and go home. ");
+       
+        String Progress = scan.nextLine();
+       
+        while(!Progress.equals("Forward") && !Progress.equals("Leave")) {
+            System.out.println("Can you read? Forward or Leave genius");
+            Progress = in.nextLine();
+        }
+     
+      if(Progress.equals("Forward")) {
+        System.out.println(" The games continue!");
+      
+        
+        if (d.MoveToRightOne()==false){
+            break;
+      }
+      }
+         else if(Progress.equals("Leave")) {
+        System.out.println(" Go home, you aren't strong enough for this adventure...puss...trami ");
+         
+      break;
+         }
+      DatagramPacket pkt = new DatagramPacket(Progress.getBytes(),Progress.length(),ip,58778); 
       sc.send(pkt);
+      
+        } 
+        System.out.println(" Ravioli Ravioli, Thanks for playing. "
+                + "(Bet you thought that would've Rhymed) ");
+        System.out.println("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+    
       sc.close();
+     
+      
+ }
 }
-}
+
+
+
+
+    
+
+                
+    
+
--- Server.java
+++ Server.java
@@ -1,31 +1,52 @@
-import java.io.IOException;
-import java.io.PrintStream;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.net.*;
-import java.util.Scanner;
-
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+package Classes;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.util.Scanner;
+import java.util.Random;
 /**
  *
  * @author Big Thomas
  */
 public class Server {
-      public static void main(String args[]) throws IOException{
+      public static void main(String[] args)throws IOException {
+        Dungeon d = new Dungeon();
+        Scanner in = new Scanner(System.in);
+         Random rand = new Random();
         
-        
-          DatagramSocket sc = new DatagramSocket(58778);
+        DatagramSocket sc = new DatagramSocket(58778);
          byte[] buf = new byte[1024];
          DatagramPacket pkt = new DatagramPacket( buf,1024);
          sc.receive(pkt);
-         String greeting = new String(pkt.getData(), 0, pkt.getLength());
-         System.out.println(greeting);
+         
+         
+         
+         String[] Compliments = {"You're doing great...not really though", "Wow, I couldnt be less impressed", " LOL, you think that's progress?", "Huh? You're still alive?", "Can you hurry up and quit please? My son's got a soccer game at 6."};
+         String Sarcastic = Compliments[rand.nextInt(Compliments.length)]; 
+        
+         System.out.println(Sarcastic);
          sc.close();
+             
+        
+   }  
+             
+         
+       
+     
+ }
+
+
+           
     
-}
-}
+
+             
+           
+
+
+
